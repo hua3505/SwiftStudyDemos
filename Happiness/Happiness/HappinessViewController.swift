74cd4cc35dee47925dde784f46d2d7d154c1bc78
hua3505 <959547664@qq.com>
2016-05-15 10:04:33 +0800

    add demo Happiness

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by wolf on 16/5/15.
+//  Copyright © 2016年 wolf. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
